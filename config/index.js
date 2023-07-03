39d119db6d5d92e5c4b818b39d7d16199d47b6dd
DinggiDing <77246590+DinggiDing@users.noreply.github.com>
2023-07-03 16:15:53 +0900

    Gatsby

--- config/index.js
+++ config/index.js
@@ -98,7 +98,7 @@ module.exports = {
     button: {
       useFileName: true,
       name: "Resume",
-      fileName: "goldenpuppy.pdf", // the file has to be placed inside the static folder at the root level
+      fileName: "slightCV.pdf", // the file has to be placed inside the static folder at the root level
       url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
     },
   },
